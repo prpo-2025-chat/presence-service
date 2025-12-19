89c8f28bae8cb4220c5e441f2af0951cbc5b9cd8
graddavid <davidgrad2k4@gmail.com>
2025-12-19 03:47:58 +0100

    docker + swagger

---
+++ api/src/main/java/com/prpo/chat/presence/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.prpo.chat.presence.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+  @Bean
+  public OpenAPI presenceServiceOpenAPI() {
+    return new OpenAPI()
+      .info(new Info()
+        .title("Presence Service API")
+        .version("v1"));
+  }
+}
